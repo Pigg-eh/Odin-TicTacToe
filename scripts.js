505272adae35937edeb553eaaf59f3d3d7bc5a33
Ed Hunter <ep.hunter23@gmail.com>
2024-05-30 17:31:32 +0800

    feat:add functionality to check if space is taken

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let grid = [] 
 
-function gameGrid (currentMarker, x, y) { 
+function gameGrid (processedPlayer, x, y) { 
 
     if (grid.length === 0){for(i=0; i< 3; i++) {
             grid[i] = [];
@@ -9,10 +9,14 @@ function gameGrid (currentMarker, x, y) {
             }
         }
     }
-    
 
     const insertMarkers = () => {
-        grid[x].splice(y, 1, currentMarker) 
+        if(grid[x][y] === ''){
+            grid[x].splice(y, 1, processedPlayer.marker)
+        } else {
+            alert('wrong input,please try again') //on wrong input helloGrid is being called twice
+            gameController.placeMarker(processedPlayer)
+        } 
     } 
 
 
@@ -47,25 +51,21 @@ function gameController () {
     toggle = toggle ? false : true;
     let currentPlayer = (toggle) ? playerArray[0] : playerArray[1]
 
-    // if (currentPlayer === playerArray[1]) { 
-    //     currentPlayer = playerArray[0]
-    // } else if(currentPlayer === playerArray[0]){
-    //     currentPlayer = playerArray[1]
-    // }
+    
     console.log(`currentplayername:${currentPlayer.name}`)
     
-    placeMarker()
-    function placeMarker(){
+    placeMarker(currentPlayer)
+    function placeMarker(processedPlayer){
         x = prompt('pick row number')
         y = prompt('pick column number')
-        const gameState = gameGrid(currentPlayer.marker, x,y) //need to save console.table
+        const gameState = gameGrid(processedPlayer, x,y) //need to save console.table
         
         gameState.insertMarkers()
         gameState.helloGrid()
 
         
     }
-
+    gameController.placeMarker = placeMarker
 
     
 
